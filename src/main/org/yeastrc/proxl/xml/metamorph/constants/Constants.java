1cc6be0dfe389596b8e5c3dead2c7ebde4dc6715
Michael Riffle <mriffle@uw.edu>
2019-03-14 12:03:57 -0700

    Update version to 1.6.1

--- src/main/org/yeastrc/proxl/xml/metamorph/constants/Constants.java
+++ src/main/org/yeastrc/proxl/xml/metamorph/constants/Constants.java
@@ -2,5 +2,5 @@ package org.yeastrc.proxl.xml.metamorph.constants;
 
 public class Constants {
     public static final String CONVERSION_PROGRAM_URI = "https://github.com/yeastrc/proxl-import-metamorpheus";
-    public static final String CONVERSION_PROGRAM_VERSION = "1.6.0";
+    public static final String CONVERSION_PROGRAM_VERSION = "1.6.1";
 }
